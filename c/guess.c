759fbb921ecc2fd4760f54f8a871288531c2b040
Zain-Khoso <zain.khoso.dev@gmail.com>
2026-02-11 23:38:09 +0500

    new: Number guessing game

---
+++ c/guess.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main()
+{
+    long number = get_int("Tell me a number to guess: ");
+
+    int steps = 0;
+    long guess = 0;
+
+    long far_left = 0;
+    long far_right = 0;
+
+    while (true)
+    {
+        printf("My guess is: %li\n", guess);
+
+        if (number < guess)
+        {
+            far_right = guess;
+
+            if (steps == 0)
+                guess = -1;
+
+            else if (far_left == 0)
+                guess *= 2;
+
+            else
+                guess = far_left + ((far_right - far_left) / 2);
+        }
+        else if (number > guess)
+        {
+            far_left = guess;
+
+            if (steps == 0)
+                guess = 1;
+
+            else if (far_right == 0)
+                guess *= 2;
+
+            else
+                guess = far_left + ((far_right - far_left) / 2);
+        }
+        else
+            break;
+
+        steps++;
+    }
+
+    printf("Steps taken: %i", steps);
+
+    return 0;
+}
