5c86588506f44a059b4c63a27567ff56b38f05b4
Zain-Khoso <zain.khoso.dev@gmail.com>
2026-02-11 12:31:15 +0500

    new: Interchanging data values

---
+++ c/ascii.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+    char letter = 'A';
+
+    printf("Letter as a 'char' (ASCII): %c\n", letter);
+    printf("Letter as an int: %i\n", letter);
+
+    return 0;
+}
